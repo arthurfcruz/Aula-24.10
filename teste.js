cb8e0697f87fb42701be140ff40b1221423a83cb
Arthur <arthurfelixcruz@gmail.com>
2022-10-24 22:59:28 -0300

    Adicionando a funcao soma

--- teste.js
+++ teste.js
@@ -1,2 +1,7 @@
 var num1 = parseInt(prompt("insira o primeiro numero"))
 var num2 = parseInt(prompt("insira o segundo numero"))
+
+function somar(num1, num2){
+    var soma = num1 + num2
+    return soma
+}
